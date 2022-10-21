6546a95937a1c8ef586e3aafc20fce90469f2915
hyeonchan <hyeonchan@pozalabs.com>
2022-10-21 11:57:38 +0900

    implement preprocess task

--- commu/midi_generator/info_preprocessor.py
+++ commu/midi_generator/info_preprocessor.py
@@ -1,3 +1,41 @@
+from typing import List, Any
+
+from commu.midi_generator.container import TransXlInputData
+from commu.preprocessor.encoder import MetaEncoder
+from commu.preprocessor.utils.container import MidiMeta
+
+
+def parse_meta(**kwargs: Any) -> MidiMeta:
+    return MidiMeta(**kwargs)
+
+
+def encode_meta(meta_encoder: MetaEncoder, midi_meta: MidiMeta) -> List[int]:
+    return meta_encoder.encode(midi_meta)
+
+
+def normalize_chord_progression(chord_progression: str) -> list[str]:
+    return chord_progression.split("-")
+
+
 class PreprocessTask:
     def __init__(self):
-        pass
+        self.input_data = None
+
+    def normalize_input_data(self, input_data: dict):
+        input_data["chord_progression"] = normalize_chord_progression(input_data["chord_progression"])
+        self.input_data = TransXlInputData(**input_data)
+
+    def preprocess(self) -> List[int]:
+        midi_meta = parse_meta(**self.input_data.dict())
+        meta_encoder = MetaEncoder()
+        encoded_meta = encode_meta(
+            meta_encoder=meta_encoder, midi_meta=midi_meta
+        )
+        return encoded_meta
+
+    def excecute(self, input_data: dict) -> List[int]:
+        if self.input_data is None:
+            self.normalize_input_data(input_data)
+
+        encoded_meta = self.preprocess()
+        return encoded_meta
