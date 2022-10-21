20ef6d7ede390dbc0a9f50d60f46f8ee56c1abb6
hyeonchan <hyeonchan@pozalabs.com>
2022-10-21 11:51:04 +0900

    set up initializing task on pipeline

--- commu/midi_generator/generate_pipeline.py
+++ commu/midi_generator/generate_pipeline.py
@@ -1,12 +1,23 @@
-from model_initilizer import ModelInitializeTask
+import torch
+
+from commu.midi_generator.container import ModelArguments
+from model_initializer import ModelInitializeTask
 from info_preprocessor import PreprocessTask
 from midi_inferrer import InferenceTask
 from sequence_postprocessor import PostprocessTask
 
 
 class MidiGenerationPipeline:
-    def __init__(self):
-        self.model_initialize_task = ModelInitializeTask()
+    def __init__(self, model_arguments: dict):
+        self.map_location = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(self.map_location)
+        self.model_args = ModelArguments(**model_arguments)
+
+        self.model_initialize_task = ModelInitializeTask(
+            model_args=self.model_args,
+            map_location=self.map_location,
+            device=self.device
+        )
         self.preprocess_task = PreprocessTask()
         self.inference_task = InferenceTask()
         self.postprocess_task = PostprocessTask()
